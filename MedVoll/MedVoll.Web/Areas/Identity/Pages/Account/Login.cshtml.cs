d4036fba0ac449b24f83138db38dd3d9cdc242f6
marcelooliveira <mclricardo@gmail.com>
2025-01-14 21:04:12 -0300

    4320-seguranca-aspnetcore-video3.2-Ataques de Sessão.mp4

--- MedVoll/MedVoll.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ MedVoll/MedVoll.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -21,11 +21,13 @@ namespace MedVoll.Web.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger)
+        public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger, UserManager<IdentityUser> userManager)
         {
             _signInManager = signInManager;
             _logger = logger;
+            _userManager = userManager;
         }
 
         /// <summary>
@@ -114,6 +116,14 @@ namespace MedVoll.Web.Areas.Identity.Pages.Account
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
                 if (result.Succeeded)
                 {
+                    // para a limpeza de dados de sessão residual:
+                    HttpContext.Session.Clear();
+
+                    // para a reemissão do cookie de autenticação após login:
+                    var user = await _userManager.FindByEmailAsync(Input.Email);
+                    await _signInManager.SignOutAsync();
+                    await _signInManager.SignInAsync(user, Input.RememberMe);
+
                     HttpContext.Session.SetString("VollMedCard", "1234.4567.7890.1234");
                     _logger.LogInformation("User logged in.");
                     return LocalRedirect(returnUrl);
