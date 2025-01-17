1b18b7d0aca28476cefe87b6160bc323f987e6c6
marcelooliveira <mclricardo@gmail.com>
2025-01-16 22:40:59 -0300

    4320-seguranca-aspnetcore-video4.2-Sanitizando Dados.mp4

--- MedVoll/MedVoll.Web/Program.cs
+++ MedVoll/MedVoll.Web/Program.cs
@@ -120,5 +120,17 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
+// Middleware para adicionar cabeçalhos de segurança contra:
+// 1. XSS (Cross-Site Scripting):
+// 2. Sniffing de MIME Type:
+app.Use(async (context, next) =>
+{
+    // Restringe fontes de conteúdo para evitar XSS.
+    context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self';");
+
+    // Previne a interpretação incorreta de MIME types.
+    context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+    await next();
+});
 
 app.Run();
